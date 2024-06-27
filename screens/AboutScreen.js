84f25366361117243613ccfe3580f85c3d1be4d4
Katya Greimes <katyagreimes@gmail.com>
2024-06-27 11:36:04 -0700

    Activity Indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -5,6 +5,7 @@ import { Avatar } from "react-native-elements";
 import { Text, ScrollView } from "react-native";
 import { useSelector } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "../components/LoadingComponent";
 
 const Mission = () => {
   return (
@@ -26,6 +27,32 @@ const Mission = () => {
 
 const AboutScreen = () => {
   const partners = useSelector((state) => state.partners);
+
+  if (partners.isLoading) {
+    return (
+      <ScrollView>
+        <Mission />
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          <Loading />
+        </Card>
+      </ScrollView>
+    );
+  }
+  if (partners.errMess) {
+    return (
+      <ScrollView>
+        <Mission />
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          <Text>{partners.errMess}</Text>
+        </Card>
+      </ScrollView>
+    );
+  }
+
   return (
     <ScrollView>
       <Mission />
