ea10c00bddeae2457c0c4a5ec3198c9e3f3986f0
Katya Greimes <katyagreimes@gmail.com>
2024-07-02 16:14:09 -0700

    Swipe and Delete Favorites

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -20,6 +20,7 @@ import { fetchPartners } from "../features/partners/partnersSlice";
 import { fetchCampsites } from "../features/campsites/campsitesSlice";
 import { fetchPromotions } from "../features/promotions/promotionsSlice";
 import { fetchComments } from "../features/comments/commentsSlice";
+import FavoritesScreen from "./FavoritesScreen";
 
 const Drawer = createDrawerNavigator();
 
@@ -118,6 +119,29 @@ const ReservationNavigator = () => {
   );
 };
 
+const FavoritesNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={({ navigation }) => ({
+          title: "Favorite Campsites",
+          headerLeft: () => (
+            <Icon
+              name="heart"
+              type="font-awesome"
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const DirectoryNavigator = () => {
   const Stack = createStackNavigator();
   return (
@@ -181,7 +205,10 @@ const Main = () => {
       <Drawer.Navigator
         initialRouteName="HomeNav"
         drawerContent={CustomDrawerContent}
-        drawerStyle={{ backgroundColor: "#CEC8FF" }}
+        screenOptions={{
+          headerShown: false,
+          drawerStyle: { backgroundColor: "#CEC8FF" },
+        }}
       >
         <Drawer.Screen
           name="HomeNav"
@@ -232,6 +259,22 @@ const Main = () => {
           }}
         />
         <Drawer.Screen
+          name="FavoritesNav"
+          component={FavoritesNavigator}
+          options={{
+            title: "My Favorites",
+            drawerIcon: ({ color }) => (
+              <Icon
+                name="heart"
+                type="font-awesome"
+                size={24}
+                iconStyle={{ width: 24 }}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Drawer.Screen
           name="AboutNav"
           component={AboutNavigator}
           options={{
@@ -249,7 +292,7 @@ const Main = () => {
         />
         <Drawer.Screen
           name="ContactNav"
-          component={ContactScreen}
+          component={ContactNavigator}
           options={{
             title: "Contact Us",
             drawerIcon: ({ color }) => (
