553e703abebd24ab5dd9f0bc98866e445b437256
Katya Greimes <katyagreimes@gmail.com>
2024-07-03 19:16:09 -0700

    Gestures Part 2

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -3,14 +3,24 @@ import { Card, Icon } from "react-native-elements";
 import { baseUrl } from "../../shared/baseUrl";
 import { CardAnimationContext } from "@react-navigation/stack";
 import * as Animatable from "react-native-animatable";
+import { useRef } from "react";
 
 const RenderCampsite = (props) => {
   const { campsite } = props;
+
+  const view = useRef();
 
   const isLeftSwipe = ({ dx }) => dx < -200;
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      view.current
+        .rubberBand(1000)
+        .then((endState) =>
+          console.log(endState.finished ? "finished" : "canceled")
+        );
+    },
     onPanResponderEnd: (e, gestureState) => {
       console.log("pan responder end", gestureState);
       if (isLeftSwipe(gestureState)) {
@@ -43,6 +53,7 @@ const RenderCampsite = (props) => {
         animation="fadeInDownBig"
         duration={2000}
         delay={1000}
+        ref={view}
         {...panResponder.panHandlers}
       >
         <Card containerStyle={styles.cardContainer}>
